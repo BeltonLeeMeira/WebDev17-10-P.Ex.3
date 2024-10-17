e9986a0fe341db1cdb724d042606cd130097f4bf
Belton <blcdmmeira@gmail.com>
2024-10-17 11:26:09 -0300

    criando o script do projeto

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+// criando slide show
+
+// declarando as variaveis
+
+let imagens =[
+    "src/assets/img1.jpg",
+    "src/assets/img2.jpg",
+    "src/assets/img3.jpg"
+];
+let i=0;
+let time=3000;
+
+function slideShow(){
+    document.getElementById("image").src=imagens[i];
+    i++;
+    if(i == imagens.length) {i=0;}
+    setTimeout("slideShow()",time)
+}
+slideShow();
+
+//cadastrando no formulário
